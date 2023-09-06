030ba80b88f05262936406dd5e70f6b8ef9d967c
iamMAHAM <abdulkabore@gmail.com>
2023-09-06 13:04:22 +0000

    starting building every function

--- POO/classes/array.py
+++ POO/classes/array.py
@@ -4,6 +4,9 @@ class Array:
     def __init__(self, *args):
         self.tableau = list(args)
 
+    def iterate(self):
+        return self.tableau
+
     def afficher(self):
         print(self.tableau)
 
@@ -25,10 +28,22 @@ class Array:
         return None
 
     def concat(self, *args):
-        if all(isinstance(t, Array) for t in args):
-            self.tableau
+        for tab in args:
+            if not isinstance(tab, Array):
+                raise TypeError("Un element suspect détécté : ", tab)
 
-        raise TypeError("Un element suspect détécté")
+            for element in tab.iterate():
+                self.tableau[self.length():] = [element]
+
+        return self.tableau
+
+    def copyWithin(self, cible, debut, fin):
+        pass
+
+    def every(self, fonction):
+        pass
 
 
 array = Array(0, "7", 4, 7, 8)
+array2 = Array(5, 5, 7)
+array3 = Array("yakasport", "diallogage", "dedy")
