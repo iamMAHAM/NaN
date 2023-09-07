145a9c01219ae212255f414a87e70757006af179
iamMAHAM <abdulkabore@gmail.com>
2023-09-07 12:03:37 +0000

    add every fill filter function

--- POO/classes/array.py
+++ POO/classes/array.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 class Array:
     def __init__(self, *args):
         self.tableau = list(args)
@@ -41,9 +42,40 @@ class Array:
         pass
 
     def every(self, fonction):
-        pass
+        for element in self.iterate():
+            if not fonction(element):
+                return False
+
+        return True
+
+    def fill(self, valeur, debut=0, fin=None):
+
+        if not fin:
+            fin = self.length()
+
+        if (not isinstance(debut, int) or not isinstance(fin, int)):
+            raise TypeError('Le début ou la fin doit être un entier')
+        for i in range(self.length()):
+            if debut <= i < fin:
+                self.tableau[i] = valeur
+
+        return self.tableau
+
+    def filter(self, fonction):
+        tab = Array()
+
+        for element in self.iterate():
+            if (fonction(element)):
+                tab.tableau[tab.length():] = [element]
+
+        return tab.iterate()
 
 
-array = Array(0, "7", 4, 7, 8)
+def test(x): return x > 2
+
+
+array = Array(9, 2, 4, 7, 8)
 array2 = Array(5, 5, 7)
 array3 = Array("yakasport", "diallogage", "dedy")
+
+print(array.filter(lambda x: x > 2))
