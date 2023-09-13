ed56eecf267e29502ac042217cb532b801a43f51
iamMAHAM <abdulkabore@gmail.com>
2023-09-13 11:14:00 +0000

    update scraper

--- scraping/scraper.py
+++ scraping/scraper.py
@@ -1,12 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
-import test
+import json
 
 url = "https://www.jumia.ci/flash-sales/"
 
 
 def getHTML(url):
-    res = requests.get(url)
+    res = requests.get(url, headers={
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
+    })
     return res.text
 
 
@@ -16,11 +18,33 @@ def parseHTML(source):
 
 
 def main():
+    articles = []
     soup = parseHTML(getHTML(url))
-    atricles = soup.select('article.prd._p._fb')
+    divs = soup.select('article.prd._p._fb')
+
+    for div in divs:
+        href = div.select_one('a').attrs['href']
+        titre = div.select_one('h3.name').text
+        price = div.select_one('div.prc').text
+        image = div.select_one('img').attrs['data-src']
+
+        lien = f'https://jumia.ci{href}'
+
+        soupDetail = parseHTML(getHTML(lien))
+
+        description = soupDetail.select_one('markup.-mhm.-pvl')
+        print(description)
+
+        articles.append({"titre": titre, "price": price, "image": image})
+
+    print(articles)
+
+    with open('data.json', 'w') as file:
+        file.write(json.dumps(articles, indent=2))
 
 
 # parseHTML(getHTML(url))
+
 
 if __name__ == '__main__':
     main()
